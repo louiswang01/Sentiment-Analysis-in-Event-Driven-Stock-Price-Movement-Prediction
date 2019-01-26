13832953a8a5d36819e458acf9c37512e62d6b77
louiswang01 <louisw2015@gmail.com>
2019-01-26 13:09:46 -0500

    cosmetic change

--- crawler/all_tickers.py
+++ crawler/all_tickers.py
@@ -12,33 +12,36 @@ def get_tickers(percent):
     """Keep the top percent market-cap companies."""
     assert isinstance(percent, int)
 
-    cap_stat, output = np.array([]), []
+    output = []
     for exchange in ["NASDAQ", "NYSE", "AMEX"]:
         url = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange="
         repeat_times = 10 # repeat downloading in case of http error
-        for _ in range(repeat_times):
+        for i in range(repeat_times):
             try:
-                print("Downloading tickers from {}...".format(exchange))
+                print("Downloading tickers from %s (%d/%d try)..." % (exchange, i+1, repeat_times))
                 response = urlopen(url + exchange + '&render=download')
                 content = response.read().decode('utf-8').split('\n')
                 for num, line in enumerate(content):
                     line = line.strip().strip('"').split('","')
                     if num == 0 or len(line) != 9:
                         continue # filter unmatched format
-                    # ticker, name, last_sale, market_cap, IPO_year, sector, industry
-                    ticker, name, _, market_cap, _, _, _ = line[0:4] + line[5:8]
-                    cap_stat = np.append(cap_stat, float(market_cap))
-                    output.append([ticker, name.replace(',', '').replace('.', ''),
-                                   exchange, market_cap])
+                    
+                    # Header: Symbol, Name, LastSale, MarketCap, ADR TSO, IPOyear, Sector, 
+                    #         Industry, Summary Quote
+                    ticker      = line[0]
+                    name        = line[1].replace(',', '').replace('.', '')
+                    market_cap  = float(line[3])
+                    output.append([ticker, name, exchange, market_cap])
                 break
             except:
                 continue
-
+    
+    cap_stat = [row[3] for row in output]
+    threshold = np.percentile(cap_stat, 100 - percent)
+    top_companies = [record for record in output if record[3] > threshold]
     with open('./input/tickerList.csv', 'w') as f:
-        for data in output:
-            market_cap = float(data[3])
-            if market_cap < np.percentile(cap_stat, 100 - percent):
-                continue
+        for data in sorted(top_companies):
+            data[3] = str(data[3])  # market cap float
             f.write(','.join(data) + '\n')
 
 
